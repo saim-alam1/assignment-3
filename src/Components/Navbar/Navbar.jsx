6d0d7a744305b29a2ca62eb40dc3f2ad5c112e17
saim-alam1 <allbehassansaim@gmail.com>
2026-03-18 08:59:48 +0600

    Fixed Navbar layout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -39,55 +39,57 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar max-w-400 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          <Link
+            to="/"
+            className="btn border-none shadow-none bg-none focus:none text-[16px] font-bold"
           >
-            {links}
-          </ul>
+            <img src={logo} alt="Logo Image" className="w-10" />
+            <span className="bg-linear-to-r from-[#632ee3] to-[#9f62f2] bg-clip-text text-transparent">
+              HERO.IO
+            </span>
+          </Link>
         </div>
-        <Link
-          to="/"
-          className="btn border-none shadow-none bg-none focus:none text-[16px] font-bold"
-        >
-          <img src={logo} alt="Logo Image" className="w-10" />
-          <span className="bg-linear-to-r from-[#632ee3] to-[#9f62f2] bg-clip-text text-transparent">
-            HERO.IO
-          </span>
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end mr-1.5">
-        <Link
-          to="https://github.com/saim-alam1"
-          target="_blank"
-          className="btn text-white text-[16px] font-semibold border-none shadow-none bg-linear-to-r from-[#632ee3] to-[#9f62f2]"
-        >
-          <BsGithub className="text-xl" />
-          <span>Contribute</span>
-        </Link>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end mr-1.5">
+          <Link
+            to="https://github.com/saim-alam1"
+            target="_blank"
+            className="btn text-white text-[16px] font-semibold border-none shadow-none bg-linear-to-r from-[#632ee3] to-[#9f62f2]"
+          >
+            <BsGithub className="text-xl" />
+            <span>Contribute</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
