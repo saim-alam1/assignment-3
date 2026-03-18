800965726751c36c8a7c7a9f9a651e533126388e
saim-alam1 <allbehassansaim@gmail.com>
2026-03-18 16:16:24 +0600

    Added Play Store & App Store Link Address In Banner Buttons

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,11 @@
 import playStore from "../../assets/play-store.png";
 import appStore from "../../assets/app-store.png";
 import bannerImg from "../../assets/hero.png";
+import { Link } from "react-router";
 
 const Banner = () => {
   return (
-    <div>
+    <section>
       {/* Text Container */}
       <div className="text-center w-[80%] mx-auto mt-20 mb-10">
         <h1 className="text-5xl font-black text-[#2f2f2f] mb-4">
@@ -23,21 +24,29 @@ const Banner = () => {
 
       {/* Buttons */}
       <div className="flex items-center justify-center gap-4">
-        <button className="btn h-auto hover:shadow-lg transition-all duration-300 bg-transparent ease-in-out py-2 px-6">
+        <Link
+          to="https://play.google.com/store/games?hl=en"
+          target="_blank"
+          className="btn h-auto hover:shadow-lg transition-all duration-300 bg-transparent ease-in-out py-2 px-6"
+        >
           <img src={playStore} alt="Play Store Image" className="w-8 mr-2" />
           Google Play
-        </button>
-        <button className="btn h-auto hover:shadow-lg transition-all duration-300 bg-transparent py-2 px-6 ease-in-out">
+        </Link>
+        <Link
+          to="https://www.apple.com/app-store/"
+          target="_blank"
+          className="btn h-auto hover:shadow-lg transition-all duration-300 bg-transparent py-2 px-6 ease-in-out"
+        >
           <img src={appStore} alt="App Store Image" className="w-8 mr-2" />
           App Store
-        </button>
+        </Link>
       </div>
 
       {/* Big Image */}
       <div className="mx-auto flex items-center justify-center mt-10">
         <img src={bannerImg} alt="Banner Image" className="w-87.5 md:w-175" />
       </div>
-    </div>
+    </section>
   );
 };
 
