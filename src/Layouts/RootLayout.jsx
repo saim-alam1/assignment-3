5c2cc3fbbd83cb90e7fd1414d1123784260b0895
saim-alam1 <allbehassansaim@gmail.com>
2026-03-18 08:20:05 +0600

    Added Footer For Now & Fixed Layout Structure

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const RootLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
+      <div className="grow max-w-400 mx-auto w-full">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
