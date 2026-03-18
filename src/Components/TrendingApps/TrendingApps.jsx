edc608cbd62d21f00fa1c44a2f9dddb32c9195d3
saim-alam1 <allbehassansaim@gmail.com>
2026-03-19 01:30:28 +0600

    Added Button In Trending Apps Component

--- src/Components/TrendingApps/TrendingApps.jsx
+++ src/Components/TrendingApps/TrendingApps.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router";
 import AppCard from "../Shared/AppCard/AppCard";
 
 const TrendingApps = ({ appsData }) => {
   return (
-    <section className="max-w-360 mx-auto w-full mt-20 mb-10">
+    <section className="max-w-360 mx-auto w-full mt-20 mb-10 flex flex-col items-center">
       <h2 className="text-[#001931] md:text-5xl text-3xl text-center font-bold mb-4">
         Trending Apps
       </h2>
@@ -16,6 +17,13 @@ const TrendingApps = ({ appsData }) => {
           <AppCard key={appData.id} appData={appData} />
         ))}
       </div>
+
+      <Link
+        to="/apps"
+        className="text-[16px] font-semibold btn border-none shadow-none bg-linear-to-br from-[#632ee3] to-[#9f62f2] text-white py-5 px-10"
+      >
+        Show All
+      </Link>
     </section>
   );
 };
